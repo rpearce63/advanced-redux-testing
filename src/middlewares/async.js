5b34c895874e5971ea8955f19c4a0e756b6090ab
Rick Pearce <rick.pearce.crzs@statefarm.com>
2019-01-03 22:09:59 -0600

    middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,5 +1,15 @@
-export default () => {
-  return () => {
-    return () => {};
-  };
+export default ({ dispatch }) => next => action => {
+  // Check to see if the action has a Promise on it's payload property
+  if (!action.payload || !action.payload.then) {
+    // If not, forward to next middleware
+    return next(action);
+  }
+
+  // If so, wait for it to resolve
+  // get it's data, then create a new action
+  // and send back thru middleware chain
+  action.payload.then(response => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
